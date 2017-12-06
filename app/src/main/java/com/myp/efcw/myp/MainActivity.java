6ca507410fe8180465225847af6d11a48a97ee5f
shhan <shhan@shhanui-MacBook-Pro.local>
2017-12-06 12:24:18 +0900

    1

--- app/src/main/java/com/myp/efcw/myp/MainActivity.java
+++ app/src/main/java/com/myp/efcw/myp/MainActivity.java
@@ -9,5 +9,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        String test = "TEST";
+        String ID = "id";
+        String Password = "password";
     }
 }
